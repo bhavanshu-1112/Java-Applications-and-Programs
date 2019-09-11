6a9fdb8767428319e5402cd44946300ea432f15e
Bhavanshu Jain <46085014+bhavanshu-1112@users.noreply.github.com>
2019-09-11 21:23:47 +0530

    Add files via upload

---
+++ javaappe/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+	public static void main(String args[]){
+	    Dog dog1 = new Dog();
+	    dog1.breed = "Greman Shaphard";
+	    dog1.color = "White";
+	    dog1.size = 45;
+
+	    Dog dog2 = new Dog(German Shephard, brown , 54);
+	}
+}
---
+++ javaappe/src/Dog.java
@@ -0,0 +1,20 @@
+public class Dog{
+    public String breed;
+    public String color;
+    public int size;
+}
+public Dog(){
+}
+     System.out.println(dog1.breed);
+     System.out.println(dog1.color);
+     System.out.println(dog1.size);
+
+
+public Dog(String b,String c,int s){
+        breed = b;
+        color = c;
+        size = s;
+        }
+        System.out.println(dog2.breed);
+        System.out.println(dog2.color);
+        System.out.println(dog2.size);
